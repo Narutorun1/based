894ea9b6e8daf663f8dcc9a3be9e12316b2946b8
Narutorun1 <147037254+Narutorun1@users.noreply.github.com>
2025-10-05 13:00:01 +0530

    Update __init__.py

---
+++ roop/processors/__init__.py
@@ -0,0 +1,24 @@
+from .run import run
+from .ui import init
+from .globals import *
+from .predictor import get_landmarks, clear_predictor
+from .face_analyser import get_one_face
+from .capturer import get_video_frame, get_video_frame_total
+from .face_reference import get_face_reference, set_face_reference, clear_face_reference
+from .processors.frame.core import get_frame_processors_modules
+from .core import *  # Add this line
+
+__all__ = [
+    'run',
+    'init',
+    'get_landmarks',
+    'clear_predictor',
+    'get_one_face',
+    'get_video_frame',
+    'get_video_frame_total',
+    'get_face_reference',
+    'set_face_reference',
+    'clear_face_reference',
+    'get_frame_processors_modules',
+    'core'  # Add to __all__
+]
